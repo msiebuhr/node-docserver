552535075fd4c5f4f73ce5d6d42a8acfdd895db4
Morten Siebuhr <msi@one.com>
2012-08-24 10:39:04 +0200

    Add server command-line configuration.

--- server.js
+++ server.js
@@ -4,9 +4,17 @@
  * Launch server
  */
 
-var appBuild = require('./lib/app');
+var appBuild = require('./lib/app'),
+    opt = require('optimist')
+        .usage('$0 [-p port] [-d directory]')
+        .options('port', { alias: 'p', default: 3000 })
+        .options('docDir', { alias: 'd', default: '/tmp/docserver/' }),
+    argv = opt.argv;
 
-var app = appBuild({
-    docDir: '/tmp/docserver/'
-});
-app.listen(3000);
+if ('help' in argv) {
+    opt.showHelp();
+    process.exit();
+}
+
+var app = appBuild(argv);
+app.listen(argv.port);
